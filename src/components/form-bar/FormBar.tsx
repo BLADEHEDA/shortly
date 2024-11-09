5c68333a60b7b2beba5aa4df050747f1e8fe26a5
Blade-heda <nkwetakembrunodudu9@gmail.com>
2024-11-09 08:38:22 +0100

    added the url shortening

--- src/components/form-bar/FormBar.tsx
+++ src/components/form-bar/FormBar.tsx
@@ -1,16 +1,46 @@
 import { Button, Form, Input, message, Row, Col } from "antd";
 import { formButtonStyle } from "../../utils/CustomStyles";
 import "./FormBar.css";
+import { useState } from "react";
 
 const FormBar = () => {
   const [form] = Form.useForm();
+  const [shortenedURL, setShortenedURL] = useState("");
 
-  const onFinish = (values: string) => {
-    // Log the form values to the console
-    console.log("Form values:", values);
+  const onFinish = async (values: { url: string }) => {
+    const urlToShorten = values.url;
 
-    // Display success message
-    message.success("Submit success!");
+    try {
+      console.log('Original URL:', urlToShorten);
+
+      // Send a POST request to the Rebrandly API for URL shortening
+      const response = await fetch("https://api.rebrandly.com/v1/links", {
+        method: "POST",
+        headers: {
+          accept: 'application/json',
+    'content-type': 'application/json',
+    apikey: '798dec9cf05d4d97aa7c320194cb6c3b'
+        },
+        body: JSON.stringify({
+          destination: urlToShorten,
+        }),
+      });
+
+      const data = await response.json();
+
+      // Check if the response contains a shortened URL
+      if (data && data.shortUrl) {
+        const shortURL = data.shortUrl;
+        setShortenedURL(shortURL); // Store the shortened URL in state
+        message.success(`Shortened URL: ${shortURL}`);
+        console.log('The shortened URL is:', shortURL);
+      } else {
+        message.error("Failed to shorten URL. Please try again.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      message.error("An error occurred while shortening the URL.");
+    }
   };
 
   const onFinishFailed = () => {
