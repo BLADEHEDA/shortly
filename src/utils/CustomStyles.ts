9c88d40135245c44b3378f000e2d445984123ca1
Blade-heda <nkwetakembrunodudu9@gmail.com>
2024-11-11 08:46:11 +0100

    adjusted the and refactore the code

--- src/utils/CustomStyles.ts
+++ src/utils/CustomStyles.ts
@@ -1,38 +1,37 @@
-export const buttonStyle = {
+const baseButtonStyle = {
   background: "#2acfcf",
   borderRadius: "30px",
   width: "100%",
   padding: "1.5em",
   fontSize: "1.2em",
+};
+
+// Refactored button styles
+export const buttonStyle = {
+  ...baseButtonStyle,
 };
 
 export const desktopButtonStyle = {
-  background: "#2acfcf",
-  borderRadius: "30px",
-  width: "100%",
+  ...baseButtonStyle,
   padding: "1.2em",
 };
+
 export const formButtonStyle = {
-  background: "#2acfcf",
-  borderRadius: "10px",
-  width: "100%",
-  padding: "1.5em",
-  fontSize: "1.2em",
+  ...baseButtonStyle,
+  borderRadius: "10px", 
   fontWeight: "900 !important",
 };
+
 export const heroButtonStyle = {
-  background: "#2acfcf",
-  borderRadius: "30px",
-  width: "100%",
-  padding: "1.2em",
-  fontSize: "1.2em",
+  ...baseButtonStyle,
+  padding: "1.2em 1.5em", 
+  boxSizing: "border-box",
 };
 
 export const shortenedButtonStyle = {
-  background: "#2acfcf",
+  ...baseButtonStyle,
   borderRadius: "10px",
-  width: "100%",
-  padding: "1em 2em",
-  fontSize: "1em",
+  padding: "1em 2em", 
+  fontSize: "1em", 
   fontWeight: "900 !important",
 };
